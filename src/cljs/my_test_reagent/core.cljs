1ee36434817867a628b33ccd326f9594b3af6908
swati <swatichauhanhjs17@gmail.com>
2020-04-14 00:58:40 +0530

    checkbox working

--- src/cljs/my_test_reagent/core.cljs
+++ src/cljs/my_test_reagent/core.cljs
@@ -1,22 +1,22 @@
 (ns my-test-reagent.core
   (:require
-   [reagent.core :as reagent :refer [atom]]
-   [reagent.session :as session]
-   [reitit.frontend :as reitit]
-   [clerk.core :as clerk]
-   [accountant.core :as accountant]))
+    [reagent.core :as reagent :refer [atom]]
+    [reagent.session :as session]
+    [reitit.frontend :as reitit]
+    [clerk.core :as clerk]
+    [accountant.core :as accountant]))
 
 ;; -------------------------
 ;; Routes
 
 (def router
   (reitit/router
-   [["/" :index]
-    ["/my-new-route" :my-new-route]
-    ["/items"
-     ["" :items]
-     ["/:item-id" :item]]
-    ["/about" :about]]))
+    [["/" :index]
+     ["/my-new-route" :my-new-route]
+     ["/items"
+      ["" :items]
+      ["/:item-id" :item]]
+     ["/about" :about]]))
 
 (defn path-for [route & [params]]
   (if params
@@ -36,7 +36,7 @@
       [:li [:a {:href (path-for :items)} "Items of my-test-reagent"]]
       [:li [:a {:href "/broken/link"} "Broken link"]]]]))
 
-
+(def my-checkbox-values (reagent/atom {:red false :orange false :blue false}))
 (def my-color-atom (reagent/atom "green"))
 (def prev-color (reagent/atom []))
 (def old-color (reagent/atom nil))
@@ -60,57 +60,96 @@
            :on-change #(reset! value (-> % .-target .-value))}])
 
 (defn change-color []
-    (fn []
-      [:div
-       [:p "Change the colour here: " [color-input my-color-atom]
-       [:p "This is your new colour: " @my-color-atom]
-       ]]))
+  (fn []
+    [:div
+     [:p "Change the colour here: " [color-input my-color-atom]
+      [:p "This is your new colour: " @my-color-atom]
+      ]]))
 
 (defn my-current-color [color]
   [:div
-    /" Current Color of button changed to :- " color]
+   " Current Color of button changed to :- " color]
   )
 
 (defn my-button []
   (fn []
-  [:div
-   [:span {:style {:background-color @old-color}} " Click here to change the colour : "
-    [:input {:type "button" :value "Click me!"
-             :on-click  #(do (reset!  old-color  @my-color-atom)
-                             (show-color @my-color-atom))}] ]
+    [:div
+     [:span {:style {:background-color @old-color}} " Click here to change the colour : "
+      [:input {:type "button" :value "Click me!"
+               :on-click  #(do (reset!  old-color  @my-color-atom)
+                               (show-color @my-color-atom))}] ] ]))
 
+(defn my-orange-button []
+  (fn []
+    [:div
+     [:span  " Click here to change the colour : "
+      [:input {:type "button" :value "ORANGE!"
+               :on-click #(do (reset!  old-color   "orange")
+                              (show-color "orange") )}] ]
+
+     ] ) )
+
+
+(defn my-blue-button []
+  (fn []
+    [:div
+     [:span  " Click here to change the colour : "
+      [:input {:type "button" :value "BLUE!"
+               :on-click #(do (reset!  old-color   "blue")
+                              (show-color "blue") )
+               }] ]] ) )
+
+(defn my-red-button []
+  (fn []
+
+    [:div
+
+     [:span  " Click here to change the colour : "
+      [:input {:type "button" :value "RED!"
+               :on-click #(do (reset!  old-color   "red")
+                              (show-color "red") )
+
+               }] ]
+
+     ]))
+
+
+
+
+(defn check-box []
+  (fn []
+    [:div
+     [:input {:type "checkbox", :id "col1", :name "red", :value "red"
+              :on-click #(swap! my-checkbox-values assoc :red(-> % .-target .-checked))  }]
+     [:label {:for "red"} " I have a red"]
+     [:br]
+     [:input {:type "checkbox", :id "col2", :name "orange", :value "orange"
+              :on-click #(swap! my-checkbox-values assoc :orange(-> % .-target .-checked))}]
+     [:label {:for "orange"} " I have a orange"]
+     [:br]
+     [:input {:type "checkbox", :id "col3", :name "blue", :value "Blue"
+              :on-click #(swap! my-checkbox-values assoc :blue(-> % .-target .-checked))}]
+     [:label {:for "blue"} " I have a blue"]
+     [:br]
+     [:br]
+     [:input {:type "submit", :value "Submit"}]]
+
+    ))
+
+(defn show-buttons [my-checkbox-values]
    [:div
-    [:span  " Click here to change the colour : "
-     [:input {:type "button" :value "ORANGE!"
-              :on-click #(do (reset!  old-color   "blue")
-                             (show-color "orange") )}] ]
+    (if (get @my-checkbox-values :red) [my-red-button]  nil)
+    (if (get @my-checkbox-values :orange) [my-orange-button]  nil)
+    (if (get @my-checkbox-values :blue) [my-blue-button]  nil)]
 
-    ]
-   [:div
-    [:span  " Click here to change the colour : "
-     [:input {:type "button" :value "BLUE!"
-              :on-click #(do (reset!  old-color   "blue")
-                             (show-color "blue") )
-              }] ]]
-
-   [:div
-
-    [:span  " Click here to change the colour : "
-     [:input {:type "button" :value "RED!"
-              :on-click #(do (reset!  old-color   "red")
-                             (show-color "red") )
-
-              }] ]
-
-    ]
-
-    ]))
+  )
 
 
 
 (defn my-new-page []
   (fn [] [:span.main
-          [:h1 "Welcome to my new page"] [change-color] [my-button] [my-current-color @old-color] [show-all-values @prev-color]
+          [:h1 "Welcome to my new page"] [change-color] [my-button]   [my-current-color @old-color] [show-all-values @prev-color] [check-box] [show-buttons my-checkbox-values]
+
           ]))
 
 (defn items-page []
@@ -177,19 +216,20 @@
 (defn init! []
   (clerk/initialize!)
   (accountant/configure-navigation!
-   {:nav-handler
-    (fn [path]
-      (let [match (reitit/match-by-path router path)
-            current-page (:name (:data  match))
-            route-params (:path-params match)]
-        (reagent/after-render clerk/after-render!)
-        (session/put! :route {:current-page (page-for current-page)
-                              :route-params route-params})
-        (clerk/navigate-page! path)
-        )
-      )
-    :path-exists?
-    (fn [path]
-      (boolean (reitit/match-by-path router path)))})
+    {:nav-handler
+     (fn [path]
+       (let [match (reitit/match-by-path router path)
+             current-page (:name (:data  match))
+             route-params (:path-params match)]
+         (reagent/after-render clerk/after-render!)
+         (session/put! :route {:current-page (page-for current-page)
+                               :route-params route-params})
+         (clerk/navigate-page! path)
+         )
+       )
+     :path-exists?
+     (fn [path]
+       (boolean (reitit/match-by-path router path)))})
   (accountant/dispatch-current!)
   (mount-root))
+
